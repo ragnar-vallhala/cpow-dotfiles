4b495d39ec93196dd5124dd1cdaabf124210a801
ragnar-vallhala <ashutoshvishwakarma208@outlook.com>
2024-09-12 23:51:38 +0530

    settings updated

--- init.lua
+++ init.lua
@@ -15,6 +15,30 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 
+-- Function to save session
+local function save_session()
+    vim.api.nvim_command('mksession! mysession.vim')
+    print("Session saved as 'mysession.vim'")
+end
+
+-- Function to load session
+local function load_session()
+    vim.api.nvim_command('source mysession.vim')
+    print("Session loaded from 'mysession.vim'")
+end
+
+-- Keybindings to save and load session
+vim.api.nvim_set_keymap('n', '<leader>ss', ':lua save_session()<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>ls', ':lua load_session()<CR>', { noremap = true, silent = true })
+
+-- Automatically load session if "mysession.vim" exists
+local session_file = 'mysession.vim'
+if vim.fn.filereadable(session_file) == 1 then
+    vim.api.nvim_command('source ' .. session_file)
+    print("Session loaded automatically from 'mysession.vim'")
+end
+
+
 require("core.options")
 require("core.keymaps")
 require("core.plugins")
